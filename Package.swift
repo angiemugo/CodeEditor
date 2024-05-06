1a410a6fbadd0ccadb0c96a6ba54d90cf8e758ec
angiemugo <angela@basecamp.com>
2024-05-06 15:21:55 +0300

    modify

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  
+  name: "CodeEditor",
+
+  platforms: [
+    .macOS(.v10_15), .iOS(.v13)
+  ],
+
+  products: [
+    .library(name: "CodeEditor", targets: [ "CodeEditor" ])
+  ],
+  
+  dependencies: [
+    .package(url: "https://github.com/apps-fab/Highlightr.git", .branch("master"))
+  ],
+           
+  targets: [
+    .target(name: "CodeEditor", dependencies: [ "Highlightr" ])
+  ]
+)
